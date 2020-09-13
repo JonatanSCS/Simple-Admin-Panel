d62be67d78045a7900cb87e0760660cc44c12880
Jonatan Santa Cruz <jonatan.santacruz@schibsted.com>
2020-09-13 15:50:44 -0500

    Init development basic setup for React app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple Admin Panel</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
