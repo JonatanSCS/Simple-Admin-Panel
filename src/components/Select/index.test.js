d4afe8e32898c6d5c5864d914ac7409e75964140
Jonatan Santa Cruz <jonatan.santacruz@schibsted.com>
2020-09-14 14:36:33 -0500

    Add general Select component

---
+++ src/components/Select/index.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Select from './index'
+
+const items = [{
+  value: 'date',
+  label: 'Date'
+}, {
+  value: 'status',
+  label: 'Payment Status'
+}, {
+  value: 'type',
+  label: 'Payment Type'
+}]
+const onChange = jest.fn()
+const wrapper = shallow(<Select items={items} value="date" onChange={onChange}/>)
+const select = wrapper.find('[data-testid="SelectMain"]')
+
+test('Page render successfully', () => {
+  const options = wrapper.find('[data-testid="SelectOption"]')
+  expect(options).toHaveLength(items.length)
+  expect(select.props().defaultValue).toEqual('date')
+})
+
+test('Select change call onChange handler', () => {
+  select.simulate('change', { target: { value: 'type' } })
+  expect(onChange).toHaveBeenCalled()
+})
--- src/views/Payments/index.js
+++ src/views/Payments/index.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select from '../../components/Select'
 
 function Payments () {
-  return <h1>Payments</h1>
+  const [sort, setSort] = useState('date')
+  const sortBy = [{
+    value: 'date',
+    label: 'Date'
+  }, {
+    value: 'status',
+    label: 'Payment Status'
+  }, {
+    value: 'type',
+    label: 'Payment Type'
+  }, {
+    value: 'custumer',
+    label: 'Custumer'
+  }, {
+    value: 'amount',
+    label: 'Amount'
+  }]
+
+  return (
+    <div className="Payments">
+      <Select items={sortBy} value={sort} onChange={setSort}/>
+    </div>
+  )
 }
 
 export default Payments
